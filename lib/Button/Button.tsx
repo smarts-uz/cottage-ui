325d493457aca5182b608f4d14e7bc21a52fa45f
Ramz001 <rkenjaev1@gmail.com>
2025-11-10 08:59:44 +0500

    update version to 0.1.2, enhance Button component with updateModel prop and console log, and remove package-lock.json

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,36 +1,53 @@
 import type { MouseEvent, ReactNode } from "react";
+import React from "react";
 
 export enum BUTTON_VARIANTS {
-	PRIMARY = 'primary',
-	DEFAULT = 'default'
+  PRIMARY = "primary",
+  DEFAULT = "default",
 }
 
 export enum BUTTON_SIZES {
-	LARGE = 'large',
-	DEFAULT = 'default'
+  LARGE = "large",
+  DEFAULT = "default",
 }
 
 const ButtonVariantStyling: Record<BUTTON_VARIANTS, string> = {
-	[BUTTON_VARIANTS.PRIMARY]: 'bg-orange-700 hover:bg-orange-500 text-white',
-	[BUTTON_VARIANTS.DEFAULT]: 'bg-gray-700 hover:bg-gray-800 text-white'
-}
-
-const ButtonSizeStyling: Record<BUTTON_SIZES, string> = {
-	[BUTTON_SIZES.DEFAULT]: '',
-	[BUTTON_SIZES.LARGE]: 'text-lg'
-}
-
-interface ButtonProps {
-	children: ReactNode;
-	onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-	variant?: BUTTON_VARIANTS;
-	size?: BUTTON_SIZES;
-}
-
-const Button = ({ children, onClick, variant = BUTTON_VARIANTS.DEFAULT, size = BUTTON_SIZES.DEFAULT }: ButtonProps): ReactNode => {
-	const className = `rounded p-2 ${ButtonVariantStyling[variant]} ${ButtonSizeStyling[size]}`
-
-	return <button onClick={onClick} className={className}>{children}</button>;
+  [BUTTON_VARIANTS.PRIMARY]: "bg-orange-700 hover:bg-orange-500 text-white",
+  [BUTTON_VARIANTS.DEFAULT]: "bg-gray-700 hover:bg-gray-800 text-white",
 };
 
-export default Button
+const ButtonSizeStyling: Record<BUTTON_SIZES, string> = {
+  [BUTTON_SIZES.DEFAULT]: "",
+  [BUTTON_SIZES.LARGE]: "text-lg",
+};
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+  variant?: BUTTON_VARIANTS;
+  size?: BUTTON_SIZES;
+  updateModel?: any;
+}
+
+const Button = ({
+  children,
+  onClick,
+  variant = BUTTON_VARIANTS.DEFAULT,
+  size = BUTTON_SIZES.DEFAULT,
+  updateModel = () => null,
+}: ButtonProps): ReactNode => {
+  const className = `rounded p-2 ${ButtonVariantStyling[variant]} ${ButtonSizeStyling[size]}`;
+
+  console.log("hello from CDN");
+  React.useEffect(() => {
+    updateModel({ data: [{ id: "hello", data: "smurfs" }] });
+  }, []);
+
+  return (
+    <button onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
