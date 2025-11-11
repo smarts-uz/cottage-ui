4e7e974edfabd627bb77c9932eb6ae07f1359e51
Ramz001 <rkenjaev1@gmail.com>
2025-11-11 21:40:35 +0500

    bump version to 0.1.2, add @tanstack/react-query and @tanstack/react-query-devtools dependencies, and refactor main module exports

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,5 @@
 import "./tailwind.css";
+import { TableQuery } from "./TableQuery/TableQuery";
 import Button from "./Button/Button";
-import { TanstackTable } from "./Table/Table";
-import { ShadcnTable } from "./TableCN/TableCN";
 
-export { Button, TanstackTable as Table, ShadcnTable as TableCN };
+export { TableQuery as Table, Button };
